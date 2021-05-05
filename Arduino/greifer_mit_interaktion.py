eacb77f2007c9ae35a67b3a0db8db8d054138e09
Johsei <elphator@online.de>
2021-05-05 20:40:40 +0200

    Robot Arm can now be controlled via python script; YoloV3 NN works

---
+++ Arduino/greifer_mit_interaktion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import serial
+import time
+if __name__ == '__main__':
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+  ser.flush()
+  while True:
+    test = input ("Testeingabe: ")
+    print("Eingabe war: ", test)
+    test2 = test + "\n"
+    print(test2)
+    test_as_bytes = str.encode(test2)
+    ser.write(test_as_bytes)
+    line  = ser.readline().decode('utf-8').rstrip()
+    print(line)
+    line  = ser.readline().decode('utf-8').rstrip()
+    print(line)
