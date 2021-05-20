bbbb30e4b030979a44c9d4d0a3c9dc63ee796442
Johsei <elphator@online.de>
2021-05-20 16:16:03 +0200

    run.py optimization

--- CRS/run.py
+++ CRS/run.py
@@ -1,24 +1,26 @@
 import argparse
 from scripts.crs import initialize, detect, deinitialize
 
-print('--------------------------------------\nCRS V1\n--------------------------------------')
+def main(args):
+	initialize()
 
-# Define show flag
-parser = argparse.ArgumentParser()
-parser.add_argument('--show', default=False, action='store_true', help='Shows the camera and sensor views on the screen')
-args = parser.parse_args()
-#print(args)
+	try:
+		while True:
+			results = detect(args.show)
+			print('\nRESULTS: ', results[0], results[1], results[2].x, results[2].y, results[3].x, results[3].y)
+	except KeyboardInterrupt:
+		pass
 
-initialize()
+	deinitialize()
 
-try:
-	while True:
-		global results
-		results = detect(args.show)
-		print('\nRESULTS: ', results[0], results[1], results[2].x, results[2].y, results[3].x, results[3].y)
-except KeyboardInterrupt:
-	pass
+	print('\n--- CRS END ---')
 
-deinitialize()
+if __name__ == "__main__":
+	print('--------------------------------------\nCRS V1\n----------------------------------------')
 
-print('\n--- CRS END ---')
+        # Define show flag
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--show', default=False, action='store_true', help='Shows the camera images on the screen')
+	args = parser.parse_args()
+
+	main(args)
